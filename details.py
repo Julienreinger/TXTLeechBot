16a1f6e3cd7e7f7fc4245f20cc190cbbd32688ae
Adrenalinators <132149283+Adrenalinators@users.noreply.github.com>
2023-05-02 10:54:38 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 28473180
-api_hash = "60cc753767c22c5b0529daf633b3a02e"
+api_id = 26435700
+api_hash = "527cf5174e120a9093611bc69d7b7709"
 bot_token = "6180767554:AAGKw-Eo6L0DXlL4BsrDmKItUewgdKGAtGo"
 
